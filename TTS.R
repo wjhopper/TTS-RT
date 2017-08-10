6b002f01cb44d47132aa5ca55864f84787af4864
whopper-cemnl <wjhopper510@gmail.com>
2017-08-10 16:26:25 -0400

    added numeric method for TTS which allows you to randomly draw memory strengths from a beta distribution with a specified mean and standard deviation.
    Check for specific item, strength and threshold column names in the matrix for TTS.matrix method.

--- TTS.R
+++ TTS.R
@@ -2,13 +2,46 @@ TTS <- function(x, ...) {
   UseMethod("TTS", x)
 }
 
-TTS.numeric <- function(stregnth_dist_properties = c(1, .5),
+TTS.numeric <- function(strength_dist_properties = c(.5, .15),
                         list_length = 10,
                         threshold = 0,
                         duration = 30,
                         constant_rate = TRUE) {
-  library(whoppeR)
-  print("numeric")
+
+  if (length(strength_dist_properties) != 2) {
+    stop("'strength_dist_properties' argument must be a numeric vector with 2 elements")
+  }
+  
+  if (length(list_length) != 1 || !is.numeric(list_length)) {
+    stop("'list_length' argument must be a numeric scalar")
+  }
+  
+  if (length(list_length) != 1 || !is.numeric(list_length)) {
+    stop("'duration' argument must be a numeric scalar")
+  }
+  
+  if (length(threshold) != 1 && length(threshold) != list_length) {
+    stop("'threshold' argument must be a scalar or same length as the value of 'list_length' argument")
+  }
+  
+  if (length(threshold) == 1) {
+    threshold <- rep(threshold, list_length)
+  }
+  
+  beta_parameters <- whoppeR::betaParams(mean = strength_dist_properties[1],
+                                         sd = strength_dist_properties[2]
+                                         )
+  memory_strengths <- rbeta(list_length,
+                            shape1 = beta_parameters$a,
+                            shape2 = beta_parameters$b
+                            )
+  memory_set <- matrix(c(1:list_length, memory_strengths, threshold),
+                       nrow = list_length,
+                       dimnames = list(NULL, c("item", "strength", "threshold"))
+                       )
+  
+  x <- TTS.matrix(.data = memory_set, duration = duration, constant_rate = constant_rate)
+  return(x)
 }
 
 TTS.data.frame <- function(.data, ...) {
@@ -18,13 +51,21 @@ TTS.data.frame <- function(.data, ...) {
     stop("All variables must be numeric")
   }
 
-  x <- TTS.matrix(as.data.frame(.data), ...)
+  x <- TTS.matrix(as.matrix(.data), ...)
   return(x)
 }
 
 TTS.matrix <- function(.data,
                        duration = 30,
                        constant_rate = TRUE) {
+  
+  if (!is.numeric(.data)) {
+    stop("input matrix must be numeric")
+  }
+  
+  if ( !all(c("item","strength","threshold") %in% colnames(.data)) ) {
+    stop("input matrix must contain named columns 'item', 'strength', and 'threshold'")
+  }
   
   sim_RT  <- matrix(NA,
                     nrow = 1000,
